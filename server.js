1b9b662e9a76c4cb551b316cdc6b9378b81a003b
Alphanikhil <nikku696969@gmail.com>
2025-03-05 01:50:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 // Load User Model
-const User = require("./js/User");
+ 
 app.use(express.static(__dirname)); // ✅ This serves all static files correctly
 
 // Routes
